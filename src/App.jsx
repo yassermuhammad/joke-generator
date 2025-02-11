29b3efbd3af7b9b88013d61934d8543e88c54ce1
Yasser Muhammad <yabdelmaksoud.c@tahakom.com>
2025-02-12 01:58:56 +0300

    Adding Prime

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import Joke from './Joke/Joke';
+import { PrimeReactProvider } from 'primereact/api';
+import "primereact/resources/themes/bootstrap4-dark-blue/theme.css";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <PrimeReactProvider>
     <div className="App">
         <h1>Joke Generator Using React and Joke API</h1>
         <Joke/>
     </div>
+    </PrimeReactProvider>
 );
 }
 
--- src/Joke/Joke.jsx
+++ src/Joke/Joke.jsx
@@ -1,21 +1,70 @@
-import { useState } from 'react'
+import { useState } from "react";
 import "./Joke.css";
-import Button from '../Button/Button';
+import Button from "../Button/Button";
+import { RadioButton } from "primereact/radiobutton";
 
 function Joke(props) {
   const [joke, setJoke] = useState("");
+  const [jokeFlags, setJokeFlags] = useState("racist");
+  const jokeOptions = [
+    {
+      id: "option1",
+      name: "NSFW",
+      value: "nsfw",
+    },
+    {
+      id: "option2",
+      name: "Religious",
+      value: "religious",
+    },
+    {
+      id: "option3",
+      name: "Political",
+      value: "political",
+    },
+    {
+      id: "option4",
+      name: "Racist",
+      value: "racist",
+    },
+    {
+      id: "option5",
+      name: "Sexist",
+      value: "sexist",
+    },
+    {
+      id: "option6",
+      name: "Explicit",
+      value: "explicit",
+    },
+  ];
   const fetchJoke = () => {
-    fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
+    fetch(`https://v2.jokeapi.dev/joke/Dark?type=single&blacklistFlags=${jokeFlags}`)
       .then((response) => response.json())
       .then((data) => setJoke(data.joke));
   };
   return (
     <div className="joke">
-        <Button callApi={fetchJoke} /> 
-        <p>{joke}</p>
+      <div className="joke-options">
+        {jokeOptions.map((option) => (
+          <div className="option-container">
+            <RadioButton
+              inputId={option.id}
+              name={option.name}
+              value={option.value}
+              onChange={(e) => setJokeFlags(e.value)}
+              checked={jokeFlags === option.value}
+            />
+            <h4 htmlFor={option.id} className="ml-2">
+              {option.name}
+            </h4>
+          </div>
+        ))}
+      </div>
+      <Button callApi={fetchJoke} />
+      <p>{joke}</p>
     </div>
-);
-
-};
+  );
+}
 
 export default Joke;
